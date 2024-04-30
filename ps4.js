eb14cc9c940e7c57f0ee2a0b78cf100c078c726d
mvskvrayuduA3 <mvskvrayudu@gmail.com>
2024-04-30 16:49:03 +0530

    mvskvr

---
+++ ps4.js
@@ -0,0 +1,26 @@
+input=2
+i=0
+while (i<input){
+     i=i+1
+    console.log(i)
+}
+
+j=input
+while(j>0){
+    console.log(j)
+      j=j-1
+}
+
+k=-(input)
+while(k<0){
+    console.log(k)
+    k=k+1
+}
+
+l=0
+m=-(input)
+while(m<l){
+    l=l-1
+    console.log(l)
+}
+
